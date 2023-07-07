0d6b47f64760211ee4868717eaa64160b46561e1
MiMi-Yup <tuivanthiet@gmail.com>
2023-07-07 21:04:58 +0700

    fix missing import

--- lib/domain/use_cases/episode_page_usecase.dart
+++ lib/domain/use_cases/episode_page_usecase.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:injectable/injectable.dart';
 import 'package:join_podcast/domain/repositories/unit_of_work.dart';
 import 'package:join_podcast/models/episode_model.dart';
+import 'package:join_podcast/models/request/episode/create_request.dart';
+import 'package:join_podcast/models/response/episode/episode_response.dart';
 
 import '../../models/response/media/media_response.dart';
 
